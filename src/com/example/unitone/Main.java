b31ac9b0e1071b85b378fa2d5304e2fa78dc2c78
maartjehagens <maartje@dharmamedia.nl>
2021-02-28 18:53:50 +0100

    update files

--- src/com/example/unitone/Main.java
+++ src/com/example/unitone/Main.java
@@ -1,11 +1,16 @@
 package com.example.unitone;
 
+import java.util.Scanner;
+
 public class Main {
     public Main() {
     }
 
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
         Building c = new Building();
         System.out.println(c.getEndResult());
+
     }
 }
