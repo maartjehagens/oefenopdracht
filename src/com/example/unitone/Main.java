be616eb666ed501a7222c007f97f23bd18c35fa1
maartjehagens <maartje@dharmamedia.nl>
2021-03-09 20:29:22 +0100

    test

--- src/com/example/unitone/Main.java
+++ src/com/example/unitone/Main.java
@@ -12,5 +12,14 @@ public class Main {                                                             
         String name = homeInput.nextLine();
         System.out.println("Hello home:" + name+ "!");
 
+        /*System.out.println("Enter hoogte");
+        String inputString = scanner.nextLine();
+
+        if (inputString == "driehonderd") {
+            System.out.println("To Big!");
+        }
+        else  {
+            System.out.println("Not To big!");
+        }*/
     }
 }
